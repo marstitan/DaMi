36c0ef92108819538da778cd3da505fea3c34f79
wfl <154863017@qq.com>
2017-03-31 12:24:53 +0800

    Init Project

---
+++ DaMi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaMi
+//
+//  Created by Mac on 3/31/17.
+//  Copyright © 2017 com.dami.maggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DaMi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaMi
+//
+//  Created by Mac on 3/31/17.
+//  Copyright © 2017 com.dami.maggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
